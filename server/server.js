c686dbeba06fdcb53c48c0946871c18d2d1b6fea
Kowshikajaisankar <kowshi1104@gmail.com>
2025-06-20 12:03:37 +0530

    update server file

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
-import express from 'express'
-import cors from 'cors'
-import 'dotenv/config'
-import connectDB from './configs/mongodb.js'
-import { clerkWebhooks } from './controllers/webhooks.js'
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import connectDB from './configs/mongodb.js';
+import { clerkWebhooks } from './controllers/webhooks.js';
 
+const app = express();
 
-const app= express()
+app.use(cors());
+app.use(express.json());
 
 const startServer = async () => {
   try {
@@ -20,14 +22,7 @@ const startServer = async () => {
 
 startServer();
 
-app.use(cors())
+app.get('/', (req, res) => res.send("API Working"));
+app.post('/clerk', clerkWebhooks);
 
-app.get('/', (req, res)=> res.send("API Working"))
-app.post('/clerk', express.json(), clerkWebhooks)
-
-const PORT = process.env.PORT || 5000
-
-app.listen(PORT, ()=>{
-    console.log(`Server is running on port ${PORT}`)
-})
-
+const PORT = process.env.PORT || 5000;
