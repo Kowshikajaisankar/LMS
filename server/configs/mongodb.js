090474cabc8c4f727a772a2cd834f013a2957055
Kowshikajaisankar <kowshi1104@gmail.com>
2025-06-21 11:59:24 +0530

    update

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -1,15 +1,7 @@
 import mongoose from "mongoose";
 
-const connectDB = async () => {
-  mongoose.connection.on('connected', () => console.log('✅ Database Connected'));
-  mongoose.connection.on('error', (err) => console.error('❌ DB Connection Error:', err));
-
-  try {
-    await mongoose.connect(`${process.env.MONGODB_URI}/lms?retryWrites=true&w=majority`);
-  } catch (error) {
-    console.error("❌ Initial DB Connection Failed:", error.message);
-    process.exit(1);
-  }
-};
-
-export default connectDB;
+const connectDB = async ()=>{
+    mongoose.connection.on('connected', ()=> console.log('Database Connected'))
+    await mongoose.connect(`${process.env.MONGODB_URI}/lms`)
+}
+export default connectDB
