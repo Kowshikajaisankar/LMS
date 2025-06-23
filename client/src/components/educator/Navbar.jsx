b57e1087b6a7d830a24fccff467fabe5881ff7b1
Kowshikajaisankar <kowshi1104@gmail.com>
2025-06-23 22:35:10 +0530

    commit

--- client/src/components/educator/Navbar.jsx
+++ client/src/components/educator/Navbar.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import { assets, dummyEducatorData } from '../../assets/assets'
+import { assets } from '../../assets/assets'
 import { Link } from 'react-router-dom'
 import { UserButton, useUser } from '@clerk/clerk-react'
 
 const Navbar = () => {
-  const educatorData = dummyEducatorData
 
   const { user } = useUser()
 
